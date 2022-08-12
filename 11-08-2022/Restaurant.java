6d56560305b7f13a0307188c5d9054adf9df56db
pallabrudra <108679258+pallabrudra@users.noreply.github.com>
2022-08-12 17:28:02 +0530

    Create Restaurant.java

---
+++ 11-08-2022/Restaurant.java
@@ -0,0 +1,9 @@
+package retaurantsystem;
+
+public class Restaurant {
+
+static String Name[] = new String[]{"Biriyani Combo","Chinise  Combo","Bada Platter","Dhosa Platter"};
+static float[] Price = new float[]{200.0f, 250.0f, 200.0f, 350.0f};
+}
+
+	
