60add4a02e45e480978fcf9d9b3541f2a7de6a8e
pallabrudra <108679258+pallabrudra@users.noreply.github.com>
2022-08-25 13:11:42 +0530

    Create NoCourseFoundException.java

---
+++ 18-08-2022/NoCourseFoundException.java
@@ -0,0 +1,9 @@
+package studentmgm;
+//create NoCourseFoundException for throw exception
+public class NoCourseFoundException extends Exception{
+
+	public NoCourseFoundException(String message) {
+		super(message);
+	}
+
+}
